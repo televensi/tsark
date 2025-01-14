da178155be06fc6a478e8011243dd6ec96b5db43
televensi <televensi@proton.me>
2025-01-14 18:55:03 -0500

    feat(index): umami integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import umami from "@yeskunall/astro-umami";
 
 export default defineConfig({
 	site: "https://televensi.github.io",
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	integrations: [
+		umami({
+			id: "644cfe13-b4f1-493a-94a3-86db27bc084b",
+			endpointUrl: "https://umami.z0x.ca",
+		}),
+	],
 });
