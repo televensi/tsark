771860e2ad941cf97d1c41802e4e2c33f9e2accf
z0x <homelab@home.arpa>
2025-01-23 19:56:54 -0500

    feat(all): v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
-import tailwindcss from "@tailwindcss/vite";
-import umami from "@yeskunall/astro-umami";
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://televensi.github.io",
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	integrations: [
-		umami({
-			id: "644cfe13-b4f1-493a-94a3-86db27bc084b",
-			endpointUrl: "https://umami.z0x.ca",
-		}),
-	],
+  integrations: [react(), tailwind()]
 });
