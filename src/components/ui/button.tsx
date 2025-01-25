0c78d2c720ce3117e3177093e973fec24591c33b
z0x <homelab@home.arpa>
2025-01-25 13:11:03 -0500

    fix(astro): fix backdrop blur spelling

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        blur: "bg-background/60 shadow backgrop-blur-lg hover:bg-accent hover:text-accent-foreground",
+        blur: "bg-background/40 shadow backdrop-blur hover:bg-accent hover:text-accent-foreground",
       },
       size: {
         default: "h-9 px-4 py-2",
