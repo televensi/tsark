af107f72592269f9a6935530f8a0394515e18b39
z0x <homelab@home.arpa>
2025-01-26 18:52:45 -0500

    fix(astro): fix vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   build: {
     format: 'file'
-  }
+  },
+  output: "static",
+  adapter: vercel({
+      webAnalytics: {
+          enabled: true,
+      },
+  }),
 });
