652a3f090f86fa39557bb2606d87b56dafdfb874
z0x <homelab@home.arpa>
2025-01-27 11:58:39 -0500

    fix(astro): remove vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,10 +9,4 @@ export default defineConfig({
   build: {
     format: 'file'
   },
-  output: "static",
-  adapter: vercel({
-      webAnalytics: {
-          enabled: true,
-      },
-  }),
 });
