ff1062b03532361029d19e92a5adaa3aaed669bc
z0x <homelab@home.arpa>
2025-01-30 21:08:29 -0500

    feat(astro): add umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    umami({
+      id: "3ce380a0-dbdf-4f18-8a30-116a39dbb0e7",
+      endpointUrl: "https://umami.z0x.ca",
+    }),
+  ],
   build: {
     format: 'file'
   },
