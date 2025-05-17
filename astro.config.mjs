9640798c9344ba3cb4788f9e8d566d62840d19db
z0x <homelab@home.arpa>
2025-05-17 12:53:35 -0400

    chore: remove all analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,12 @@
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import umami from "@yeskunall/astro-umami";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(),
-    umami({
-      id: "3ce380a0-dbdf-4f18-8a30-116a39dbb0e7",
-      endpointUrl: "https://umami.z0x.ca",
-    }),
-  ],
-  build: {
-    format: 'file'
-  },
+	integrations: [react(), tailwind()],
+	build: {
+		format: "file",
+	},
 });
